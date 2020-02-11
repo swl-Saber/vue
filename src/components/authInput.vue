fffebf85a1b7c34e0259a7690393ebaa1b05381a
Potter <shenwanlin2007@163.com>
2020-02-11 21:07:00 +0800

    封装组件input输入框

---
+++ src/components/authInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="input-padding">
+    <input type="text" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.input-padding {
+  padding: 6.389vw;
+  input {
+    width: 100%;
+    height: 10.556vw;
+    box-sizing: border-box;
+    border: none;
+    border-bottom: 1px solid black;
+    background-color: #fff;
+    outline: none;
+  }
+}
+</style>
