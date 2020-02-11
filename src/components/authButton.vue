9a7282b69d1eb767745043ee2168f646123e29ad
Potter <shenwanlin2007@163.com>
2020-02-11 22:46:53 +0800

    封装登录按钮组件

---
+++ src/components/authButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="btnpadding">
+    <div class="button" @click="sendmsg">{{ text }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["text"],
+  methods: {
+    loginbtn() {
+      this.$emit("sendmsg", event);
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.btnpadding {
+  padding: 6.389vw;
+  .button {
+    width: 100%;
+    height: 13.889vw;
+    line-height: 13.889vw;
+    border-radius: 13.889vw;
+    // box-sizing: border-box;
+    outline: none;
+    border: none;
+    background-color: #cc3300;
+    text-align: center;
+    color: #fff;
+    font-size: 5.556vw;
+  }
+}
+</style>
