be2c71ebb57952951b836ccadbf4973a79fbf2b9
Potter <shenwanlin2007@163.com>
2020-02-12 23:04:21 +0800

    弹出框错误提醒，动态传入校验正则表达式，动态绑定 class 变更样式

--- src/components/authInput.vue
+++ src/components/authInput.vue
@@ -1,18 +1,60 @@
 <template>
   <div class="input-padding">
-    <input :type="itype" :placeholder="iplaceholder" />
+    <input
+      :class="{ err: !isValid, success: isValid }"
+      :type="itype"
+      :placeholder="iplaceholder"
+      @input="checkValue"
+      @blur="showAlert"
+    />
+    <!-- 监控失去焦点事件
+    如果失去焦点(输入完毕)
+    这个input的数据还是不合法
+    需要弹窗提醒-->
   </div>
+  <!-- 根据合法性判断添加动态class -->
 </template>
 
 <script>
 export default {
-  props: ["itype", "iplaceholder"]
+  data() {
+    return {
+      isValid: false //一开始就false
+    };
+  },
+  props: ["itype", "iplaceholder", "irule", "errMsg"],
+  methods: {
+    checkValue(event) {
+      // console.log("输入框的值发生了变化");
+      // console.log(event);
+      // console.log(event.target);
+      console.log(event.target.value); //获取到输入框的值
+      const regExp = new RegExp(this.irule); //代表任意字符3-10以内都是合法
+      // console.log(this.irule);
+
+      this.isValid = regExp.test(event.target.value); //test检索正则表达式与输入的字符是否匹配
+      // console.log(this.isValid);
+    },
+    showAlert() {
+      //这里是失去焦点时触发的函数
+      //需要判断，如果this.isValid 为false
+      //证明不合法，需要弹窗
+      //如果合法，什么都不做
+      if (!this.isValid) {
+        //这里的处理可以根据自己的喜好，贴近用户即可
+        //正常这里的提醒方式，是产品或设计共同决定的
+        // alert(this.errMsg); //因为输入框需求不一样，需要传值
+        this.$toast(this.errMsg);
+      }
+    }
+  }
 };
 </script>
 
 <style lang="less" scoped>
 .input-padding {
   padding: 6.389vw;
+  padding-top: 0;
   input {
     width: 100%;
     height: 10.556vw;
@@ -22,6 +64,12 @@ export default {
     background-color: #fff;
     outline: none;
     font-size: 5.556vw;
+  }
+  .err {
+    border-color: red;
+  }
+  .success {
+    border-color: #666;
   }
 }
 </style>
