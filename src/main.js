ef1f737908629aedf9e5bfc30c19b05e2aa28053
Potter <shenwanlin2007@163.com>
2020-02-12 23:00:35 +0800

    引入vant库

--- src/main.js
+++ src/main.js
@@ -1,19 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+//引入vant库
+import Vant from "vant";
+Vue.config.productionTip = false;
+//注册组件库
+Vue.use(Vant);
 
-Vue.config.productionTip = false
-
+//引入组件样式
+import "vant/lib/index.css";
 const app = new Vue({
   el: "#app",
   router,
   render(createElement) {
-    return createElement(App)
-
+    return createElement(App);
   }
   //附加：
   //为啥这个可以替换掉版本的渲染内容
   //将render当成一个过滤器来看
   //接受一个默认的渲染函数
   //返回一个我们需要的带上单文件组件的渲染函数
-})
+});
