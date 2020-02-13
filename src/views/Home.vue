3cb96f0a236d341a5cd96fe38dd0804ea2c2a1d5
Potter <shenwanlin2007@163.com>
2020-02-13 14:35:38 +0800

    在首页加入注册页链接

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
     这里是首页
-    <router-link to="./login">登录页</router-link>
+    <router-link to="./login">登录页</router-link> <br />
+    <router-link to="./register">注册页</router-link>
   </div>
 </template>
 
