be47fbb897fda774fbdecdbecc54b23edeb765a0
Potter <shenwanlin2007@163.com>
2020-02-13 21:34:06 +0800

    添加个人中心链接

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,8 @@
   <div class="home">
     这里是首页
     <router-link to="./login">登录页</router-link> <br />
-    <router-link to="./register">注册页</router-link>
+    <router-link to="./register">注册页</router-link> <br />
+    <router-link to="./profile">个人中心页</router-link>
   </div>
 </template>
 
