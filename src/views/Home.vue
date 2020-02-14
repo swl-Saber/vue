81658673a21eb662863d3fa70deace1387184d8f
Potter <shenwanlin2007@163.com>
2020-02-14 20:10:05 +0800

    主页添加退出登录按钮

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,29 @@
 <template>
   <div class="home">
     这里是首页
-    <router-link to="./login">登录页</router-link> <br />
-    <router-link to="./register">注册页</router-link> <br />
+    <br />
+    <router-link to="./login">登录页</router-link>
+    <br />
+    <router-link to="./register">注册页</router-link>
+    <br />
     <router-link to="./profile">个人中心页</router-link>
+    <br />
+    <div @click="editlogin">退出登录</div>
+    <br />
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    //退出登录
+    editlogin() {
+      localStorage.removeItem("token");
+      localStorage.removeItem("user_id");
+      console.log("退出成功");
+    }
+  }
+};
 </script>
 
 <style></style>
