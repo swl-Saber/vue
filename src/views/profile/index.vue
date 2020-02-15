f6feee569555e3c8c566681c143276949adc4080
Potter <shenwanlin2007@163.com>
2020-02-15 22:23:08 +0800

    个人中心数据渲染

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -1,17 +1,16 @@
 <template>
-  <div class="bgi">
+  <div v-if="userdata">
     <div class="profile">
-      <img
-        src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1581674911940&di=173affc623e7096b3c38656870f841a4&imgtype=0&src=http%3A%2F%2Fn.sinaimg.cn%2Fsinacn20117%2F560%2Fw1080h1080%2F20190424%2F1ee9-hvvuiyn8653738.jpg"
-        alt
-        class="pic"
-      />
+      <img v-if="userdata.head_img" :src="$axios.defaults.baseURL+userdata.head_img" class="pic" />
+      <!-- 数据库里的图片地址 -->
+      <img v-else src="@/assets/timg.jpg" class="pic" />
       <div class="profilemid">
         <div class="name">
-          <span class="iconfont iconxingbienv"></span>
-          火星网友
+          <span v-if="userdata.gender==1" class="iconfont iconxingbienv"></span>
+          <span v-else class="iconfont iconxingbienan"></span>
+          {{userdata.nickname}}
         </div>
-        <div class="time">2019-10-10</div>
+        <div class="time">{{(userdata.create_date||'').split('T')[0]}}</div>
       </div>
       <div class="arrow">
         <span class="iconfont iconjiantou1"></span>
@@ -34,7 +33,7 @@ export default {
   },
   data() {
     return {
-      userdata: null
+      userdata: {}
     };
   },
   mounted() {
@@ -56,48 +55,50 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.bgi {
-  .profile {
+.profile {
+  display: flex;
+  padding-top: 8.333vw;
+  padding-bottom: 8.333vw;
+
+  border-bottom: 5px solid #eee;
+  .pic {
+    width: 20vw;
+    border-radius: 50%;
+    margin-left: 6.944vw;
+  }
+  .profilemid {
     display: flex;
-    padding-top: 8.333vw;
-    padding-bottom: 8.333vw;
-
-    border-bottom: 5px solid #eee;
-    .pic {
-      width: 20vw;
-      border-radius: 50%;
-      margin-left: 6.944vw;
-    }
-    .profilemid {
-      display: flex;
-      flex: 1;
-      flex-direction: column;
-      // justify-content: center;
-      margin-top: 5vw;
-      margin-left: 4.167vw;
-      .name {
-        font-size: 4.444vw;
-        .iconxingbienv {
-          font-size: 5vw;
-          color: #ff468a;
-        }
+    flex: 1;
+    flex-direction: column;
+    // justify-content: center;
+    margin-top: 5vw;
+    margin-left: 4.167vw;
+    .name {
+      font-size: 4.444vw;
+      .iconxingbienv {
+        font-size: 5vw;
+        color: #ff468a;
       }
-      .time {
-        font-size: 3.889vw;
-        color: #ccc;
-        margin-top: 2.222vw;
+      .iconxingbienan {
+        font-size: 5vw;
+        color: blue;
       }
     }
-    .arrow {
-      display: flex;
-      align-items: center;
-      margin-right: 2.778vw;
-      .iconjiantou1 {
-        color: #e4e4e4;
-        font-size: 3.889vw;
+    .time {
+      font-size: 3.889vw;
+      color: #ccc;
+      margin-top: 2.222vw;
+    }
+  }
+  .arrow {
+    display: flex;
+    align-items: center;
+    margin-right: 2.778vw;
+    .iconjiantou1 {
+      color: #e4e4e4;
+      font-size: 3.889vw;
 
-        // flex-direction:row;
-      }
+      // flex-direction:row;
     }
   }
 }
