9922c848d33428bebc478d7c0eb1107385e089d7
Potter <shenwanlin2007@163.com>
2020-02-15 20:54:54 +0800

    全局拦截器捕获错误，单独引入和全局引入vant

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-//引入vant库
-import Vant from "vant";
+
 //引入axios
 import axios from "axios";
+//绑定到原型
+Vue.prototype.$axios = axios;
+
 // 设置基准路径
 axios.defaults.baseURL = "http://liangwei.tech:3000"
 //如果是本地路径
@@ -14,28 +16,48 @@ axios.defaults.baseURL = "http://liangwei.tech:3000"
 //axios.intercetors 这是拦截器的设置
 //分成两种，一个是请求拦截，一个是响应拦截
 //现在先用响应拦截
-//叫 axios.intercetors.response.use(),带一个回调函数用来处理接受到的响应
-axios.intercetors.response.use(res => {
+//叫 axios.interceptors.response.use(),带一个回调函数用来处理接受到的响应
+
+//单独引入vant-ui的一个组件
+import {
+  Toast
+} from "vant";
+Vue.use(Toast)
+//全局拦截响应
+axios.interceptors.response.use(res => {
   console.log('拦截了响应');
   //既然在这里拦截了就可以先同意处理错误
   //这里可以获取到响应
   console.log(res.data);
   //判断错误处理
-const {statusCode}=res.data;
-const errorRegExp=/^4\d\d$/
+  const {
+    statusCode,
+    message
+  } = res.data;
+  const errorRegExp = /^4\d\d$/;
+  if (statusCode && errorRegExp.test(statusCode)) {
+    //这里是全局错误处理逻辑
+    //需要弹窗
+    //但是这里是main.js甚至还没有创建vue还没有引入vant-ui
+    //所以 以前用的this.$toast.fail()
+    // console.log('这里是错误处理');
+    Toast.fail(message || '系统错误')
+  }
   //最终必须return 你的res这样整个请求才能继续处理下去
   return res;
-
 })
-//绑定到原型
-Vue.prototype.$axios = axios;
+
 
 Vue.config.productionTip = false;
+
+//全局引入vant库
+import Vant from "vant";
 //注册组件库
 Vue.use(Vant);
-
 //引入组件样式
 import "vant/lib/index.css";
+
+
 const app = new Vue({
   el: "#app",
   router,
