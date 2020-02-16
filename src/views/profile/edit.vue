da01f3c2ee7a26d6914dc29d7e45578fe7e43d98
Potter <shenwanlin2007@163.com>
2020-02-16 22:19:07 +0800

    下拉菜单性别修改功能完成

--- src/views/profile/edit.vue
+++ src/views/profile/edit.vue
@@ -7,8 +7,8 @@
     </div>
     <tabbar tableft="昵称" :tabright="userdata.nickname" @handleClick="isShownickname=true"></tabbar>
     <tabbar tableft="密码" tabright="******" @handleClick="isShowpassword=true"></tabbar>
-    <tabbar v-if="userdata.gender==1" tableft="性别" tabright="男"></tabbar>
-    <tabbar v-else tableft="性别" tabright="女"></tabbar>
+    <tabbar v-if="userdata.gender==1" tableft="性别" tabright="男" @handleClick="isShowgender=true"></tabbar>
+    <tabbar v-else tableft="性别" tabright="女" @handleClick="isShowgender=true"></tabbar>
     <!-- 昵称弹窗提示 -->
     <van-dialog
       v-model="isShownickname"
@@ -27,6 +27,16 @@
     >
       <van-field v-model="password" type="password" placeholder="请输入密码" />
     </van-dialog>
+    <!-- 下拉菜单 -->
+    <!-- v-model 控制是否显示
+    :actions 是选项数组, 里面每一个元素都是选项的对象
+    @select 是选择后的回调函数-->
+    <van-action-sheet
+      v-model="isShowgender"
+      :actions="listGender"
+      cancel-text="取消"
+      @select="onSelect"
+    ></van-action-sheet>
   </div>
 </template>
 
@@ -44,8 +54,17 @@ export default {
       userdata: {},
       isShownickname: false,
       isShowpassword: false,
+      isShowgender: false,
       nickname: "",
-      password: ""
+      password: "",
+      listGender: [
+        {
+          name: "男"
+        },
+        {
+          name: "女"
+        }
+      ]
     };
   },
 
@@ -86,9 +105,16 @@ export default {
           Authorization: localStorage.getItem("token")
         }
       }).then(res => {
-        console.log(res);
+        // console.log(res);
         this.loadPage();
       });
+    },
+    onSelect(item) {
+      console.log(item);
+      this.confirmData({
+        gender: item.name == "男" ? 1 : 0
+      });
+      this.isShowgender = false;
     }
     // confirmNickname() {
     //   console.log("点击了确认" + this.nickname);
