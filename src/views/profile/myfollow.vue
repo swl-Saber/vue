061ca051ccb4b3daf2096005dad904770289d792
Potter <shenwanlin2007@163.com>
2020-02-17 17:20:08 +0800

    关注页面css样式完成

--- src/views/profile/myfollow.vue
+++ src/views/profile/myfollow.vue
@@ -1,8 +1,58 @@
-<template></template>
+<template>
+  <div>
+    <topnav title="我的关注"></topnav>
+    <div class="item">
+      <img src="@/assets/timg.jpg" alt />
+      <div class="mid">
+        <div class="name">火星新闻播报</div>
+        <div class="time">2019-10-10</div>
+      </div>
+      <div class="follow">取消关注</div>
+    </div>
+  </div>
+</template>
 
 <script>
-export default {};
+import topNav from "@/components/topNav.vue";
+export default {
+  components: {
+    topnav: topNav
+  }
+};
 </script>
 
-<style>
+<style lang="less" scoped>
+.item {
+  display: flex;
+  border-bottom: 1px #ccc solid;
+  padding: 2.778vw 0;
+  img {
+    width: 20vw;
+    height: 20vw;
+    border-radius: 50%;
+    margin: 2.778vw;
+  }
+  .mid {
+    flex: 1;
+    margin-top: 5.556vw;
+    .name {
+      font-size: 4.444vw;
+    }
+    .time {
+      font-size: 3.889vw;
+      color: rgb(155, 155, 155);
+      margin-top: 1.389vw;
+    }
+  }
+  .follow {
+    font-size: 3.889vw;
+    display: flex;
+    border-radius: 6.944vw;
+    background-color: #fd93ba;
+    align-items: center;
+    padding-left: 2.778vw;
+    padding-right: 2.778vw;
+    margin: 7.5vw 2.778vw;
+  }
+}
 </style>
