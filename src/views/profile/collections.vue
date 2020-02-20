93f63acb2d3ec7e70fe3479a5fe7a8c88bea8bbb
Potter <shenwanlin2007@163.com>
2020-02-20 18:26:46 +0800

    我的收藏页面渲染完毕

--- src/views/profile/collections.vue
+++ src/views/profile/collections.vue
@@ -1,13 +1,40 @@
 <template>
-  
+  <div>
+    <topnav title="我的收藏"></topnav>
+    <post :post="item" v-for="(item,index) of postList" :key="index"></post>
+  </div>
 </template>
 
 <script>
+import topNav from "@/components/topNav.vue";
+import Post from "@/components/post.vue";
 export default {
-
-}
+  data() {
+    return {
+      postList: []
+    };
+  },
+  mounted() {
+    this.loadPage();
+  },
+  components: {
+    topnav: topNav,
+    post: Post
+  },
+  methods: {
+    loadPage() {
+      this.$axios({
+        url: "/user_star",
+        method: "get"
+      }).then(res => {
+        console.log(res);
+        const { data } = res.data;
+        this.postList = data;
+      });
+    }
+  }
+};
 </script>
 
 <style>
-
 </style>
