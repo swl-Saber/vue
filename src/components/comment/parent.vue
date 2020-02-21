fc276a619ecab60842f779af3b1ff3667832cd72
Potter <shenwanlin2007@163.com>
2020-02-22 00:18:00 +0800

    创建楼层parent组件，并在评论组件中渲染，实现递归

---
+++ src/components/comment/parent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Parent :parent="parent.parent" v-if="parent.parent">{{parent.parent}}</Parent>
+    <div class="parentWrapper">{{parent.content}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Parent",
+  props: ["parent"]
+};
+</script>
+
+<style lang="less" scoped>
+.parentWrapper {
+  padding: 10px;
+  border: 1px solid #ccc;
+  background-color: #eee;
+}
+</style>
