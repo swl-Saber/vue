b02024be9b9b17a575d88ad7fbac2418f1bf2619
Potter <shenwanlin2007@163.com>
2020-02-22 01:54:12 +0800

    单层评论和parent组件结构和css样式渲染完毕

--- src/components/comment/comment.vue
+++ src/components/comment/comment.vue
@@ -1,7 +1,23 @@
 <template>
   <div class="commentWrapper">
+    <div class="infoWrapper">
+      <img
+        v-if="comment.user.head_img"
+        :src="comment.user.head_img+$axios.defaults.baseURL"
+        alt
+        class="pic"
+      />
+      <img v-else src="@/assets/timg.jpg" alt class="pic" />
+      <div class="info">
+        <div class="name">{{comment.user.nickname}}</div>
+        <div class="time">2小时前</div>
+      </div>
+      <div class="btnReply">回复</div>
+    </div>
+
     <parent :parent="comment.parent" v-if="comment.parent"></parent>
-    {{comment.content}}
+
+    <div class="mainComment">{{comment.content}}</div>
   </div>
 </template>
 
@@ -17,7 +33,40 @@ export default {
 
 <style lang="less" scoped>
 .commentWrapper {
-  padding: 5.556vw 2.778vw;
   border-bottom: 1px solid #888;
+  padding: 5.556vw 4.167vw;
+  .infoWrapper {
+    display: flex;
+    // padding: 5.556vw 4.167vw;
+    .pic {
+      width: 10vw;
+      height: 10vw;
+      object-fit: cover;
+      border-radius: 50%;
+    }
+    .info {
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      margin: 0 2.778vw;
+      .name {
+        font-size: 3.611vw;
+      }
+      .time {
+        font-size: 2.778vw;
+        color: rgb(122, 122, 122);
+      }
+    }
+    .btnReply {
+      font-size: 3.889vw;
+      color: #888;
+    }
+  }
+
+  .mainComment {
+    margin-top: 4.167vw;
+    font-size: 3.611vw;
+  }
 }
 </style>
--- src/components/comment/parent.vue
+++ src/components/comment/parent.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
     <Parent :parent="parent.parent" v-if="parent.parent">{{parent.parent}}</Parent>
-    <div class="parentWrapper">{{parent.content}}</div>
+
+    <div class="parentWrapper">
+      <div class="infoWrapper">
+        <div class="name">{{parent.user.nickname}}</div>
+        <div class="time">2小时前</div>
+        <div class="btnReply">回复</div>
+      </div>
+      <div class="mainContent">{{parent.content}}</div>
+    </div>
   </div>
 </template>
 
@@ -14,8 +22,31 @@ export default {
 
 <style lang="less" scoped>
 .parentWrapper {
-  padding: 10px;
+  padding: 2.778vw;
   border: 1px solid #ccc;
   background-color: #eee;
+
+  .infoWrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .name {
+      font-size: 3.611vw;
+      margin-right: 1.389vw;
+    }
+    .time {
+      flex: 1;
+      font-size: 2.778vw;
+      color: rgb(122, 122, 122);
+    }
+    .btnReply {
+      font-size: 3.889vw;
+      color: #888;
+    }
+  }
+  .mainContent {
+    margin-top: 2.778vw;
+    font-size: 3.611vw;
+  }
 }
 </style>
