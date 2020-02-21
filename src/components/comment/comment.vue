f38fdb3f4845da0b061c0264853defc6a28a710e
Potter <shenwanlin2007@163.com>
2020-02-22 00:17:11 +0800

    创建评论组件渲染数据

---
+++ src/components/comment/comment.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="commentWrapper">
+    <parent :parent="comment.parent" v-if="comment.parent"></parent>
+    {{comment.content}}
+  </div>
+</template>
+
+<script>
+import Parent from "./parent.vue";
+export default {
+  props: ["comment"],
+  components: {
+    parent: Parent
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.commentWrapper {
+  padding: 5.556vw 2.778vw;
+  border-bottom: 1px solid #888;
+}
+</style>
