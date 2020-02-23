e902006318fbd876a5393072787ea421cc93c8c4
Potter <shenwanlin2007@163.com>
2020-02-23 22:03:26 +0800

    发送搜索请求实现

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -3,30 +3,40 @@
     <div class="searchNav">
       <span class="iconfont iconjiantou2"></span>
       <div class="searchWrapper">
-        <span class="iconfont iconsearch"></span>
-        <input type="text" placeholder="请输入搜索内容" />
+        <span class="iconfont iconsearch" @click="gobackPage"></span>
+        <input type="text" placeholder="请输入搜索内容" v-model="keyword" />
       </div>
-      <div class="btnSearch">搜索</div>
+      <div class="btnSearch" @click="sendKeyword">搜索</div>
     </div>
-    <div class="history">
-      <h3>历史记录</h3>
-      <div class="list">
-        <div v-for="(item,index) of history" :key="index">{{item}}</div>
+    <!-- 历史记录和热门搜索 -->
+    <div class="suggestions" v-if="postList.length==0">
+      <div class="history">
+        <h3>历史记录</h3>
+        <div class="list">
+          <div v-for="(item,index) of history" :key="index">{{item}}</div>
+        </div>
+      </div>
+      <div class="hotSearch">
+        <h3>热门搜索</h3>
+        <div class="list">
+          <div v-for="(item,index) of hotSearch" :key="index">{{item}}</div>
+        </div>
       </div>
     </div>
-    <div class="hotSearch">
-      <h3>热门搜索</h3>
-      <div class="list">
-        <div v-for="(item,index) of hotSearch" :key="index">{{item}}</div>
-      </div>
+    <!-- 搜索结果渲染出来的文章 -->
+    <div class="result" v-else>
+      <post :post="item" v-for="(item,index) of postList" :key="index"></post>
     </div>
   </div>
 </template>
 
 <script>
+import Post from "@/components/post.vue";
 export default {
   data() {
     return {
+      keyword: "",
+      postList: [],
       history: [
         "美女",
         "美女",
@@ -41,6 +51,39 @@ export default {
       ],
       hotSearch: ["苹果 iPhone", "小米 11", "华为荣耀"]
     };
+  },
+  components: {
+    post: Post
+  },
+  watch: {
+    keyword(newVal) {
+      //如果清空关键字，应该清除搜索结果
+      if (newVal == "") {
+        this.postList = [];
+      }
+    }
+  },
+  methods: {
+    sendKeyword() {
+      this.$axios({
+        url: "/post_search",
+        method: "get",
+        params: { keyword: this.keyword }
+      }).then(res => {
+        console.log(res.data);
+        const { data } = res.data;
+        this.postList = data;
+      });
+    },
+    //返回上一页的时候清空关键词，清空搜索结果
+    gobackPage() {
+      if (this.postList.length > 0) {
+        this.keyword = "";
+        this.postList = [];
+      } else {
+        this.$router.back();
+      }
+    }
   }
 };
 </script>
