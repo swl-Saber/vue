aba5e6bbb08c864a512d081e8784ec3f70cf8ab9
Potter <shenwanlin2007@163.com>
2020-02-24 15:03:15 +0800

    我的跟帖基本功能实现完毕

--- src/views/MyComment.vue
+++ src/views/MyComment.vue
@@ -2,15 +2,21 @@
   <div>
     <topnav title="我的跟帖"></topnav>
 
-    <div class="commentList">
+    <div class="commentList" v-for="(item,index) of commentList" :key="index">
       <div class="item">
-        <div class="date">2019-10-10</div>
-        <div class="parent">
-          <div class="name">回复:火星彩票研究员</div>
-          <div class="parentComment">阿信是张信哲吗？张信哲是不是张学友弟弟</div>
+        <div class="date">
+          {{item.create_date.split("T")[0]}}
+          <div class="time">{{item.create_date.slice(11,16)}}</div>
         </div>
-        <div class="content">阿信是张信哲吗？张信哲是不是张学友弟弟</div>
-        <div class="post">原文：阿信分享xxxxxxxxxxxxxx</div>
+        <div class="parent" v-if="item.parent">
+          <div class="name">回复:{{item.parent.user.nickname}}</div>
+          <div class="parentComment">{{item.parent.content}}</div>
+        </div>
+        <div class="content">{{item.content}}</div>
+        <div class="postWrapper">
+          <div class="post">{{item.post.title}}</div>
+          <span class="iconfont iconjiantou1"></span>
+        </div>
       </div>
     </div>
   </div>
@@ -19,8 +25,28 @@
 <script>
 import topNav from "@/components/topNav.vue";
 export default {
+  data() {
+    return {
+      commentList: []
+    };
+  },
   components: {
     topnav: topNav
+  },
+  mounted() {
+    this.loadPage();
+  },
+  methods: {
+    loadPage() {
+      this.$axios({
+        url: "/user_comments",
+        method: "get"
+      }).then(res => {
+        console.log(res.data);
+        const { data } = res.data;
+        this.commentList = data;
+      });
+    }
   }
 };
 </script>
@@ -28,10 +54,14 @@ export default {
 <style lang="less" scoped>
 .item {
   padding: 5.556vw;
-  border-bottom: 1px solid #ccc;
+  border-bottom: 1px solid rgb(124, 124, 124);
   .date {
+    display: flex;
     font-size: 4.444vw;
     color: #888;
+    .time {
+      padding-left: 2.778vw;
+    }
   }
   .parent {
     padding: 3.889vw;
@@ -51,9 +81,22 @@ export default {
     font-size: 4.444vw;
     padding: 2.778vw 0;
   }
-  .post {
-    font-size: 3.889vw;
-    color: #888;
+  .postWrapper {
+    position: relative;
+    .post {
+      font-size: 3.889vw;
+      color: #888;
+      padding-right: 8.333vw;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+      overflow: hidden;
+    }
+    .iconjiantou1 {
+      position: absolute;
+      right: -2.778vw;
+      top: 0.833vw;
+      font-size: 4.167vw;
+    }
   }
 }
 </style>
